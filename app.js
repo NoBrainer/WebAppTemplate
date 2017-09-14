4665f69fcc88b981a7cfe48f4c3fe3a956637ca3
Vincent Incarvite <vinniemac189@gmail.com>
2017-09-13 21:39:04 -0400

    Renamed the 'routes' directory to 'api'

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./api/index');
+var users = require('./api/users');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/', index);
+app.use('/api', index);
 app.use('/api/users', users);
 
 // catch 404 and forward to error handler

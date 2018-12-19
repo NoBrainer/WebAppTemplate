cbd01f82fddd70c87df2425b30e168060a615328
NoBrainer <vincent.incarvite@gmail.com>
2018-12-18 22:59:15 -0500

    Fixed scaffolding for Tab One

--- public/js/view/content/tab1/tab1View.js
+++ public/js/view/content/tab1/tab1View.js
@@ -1,7 +1,7 @@
 var templates = require('./tab1View.html');
 
 var Tab1View = Backbone.View.extend({
-    className: 'tab1View',
+    className: 'g_pageContainer tab1View',
 
     render: function() {
         var html = templates.scaffold();

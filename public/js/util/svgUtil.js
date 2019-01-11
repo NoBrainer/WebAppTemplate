733907455941f17871cc2609e38d3ebe565a7da3
NoBrainer <vincent.incarvite@gmail.com>
2019-01-10 20:50:19 -0500

    Fixed the inline SVG mechanism

--- public/js/util/svgUtil.js
+++ public/js/util/svgUtil.js
@@ -2,23 +2,11 @@
 
 const ExportedObj = module.exports = {};
 
-const elementId = 'all_svg';
-
 const SvgUtil = {
     setup: function() {
-        $.get('/resources/icons.svg')
-            .done((resp) => {
-                // Remove SVG in case it already exists
-                $('#'+elementId).remove();
-
-                // Setup the SVG element to have an id and be invisible
-                let svgElement = resp.children[0];
-                svgElement.id = elementId;
-                svgElement.setAttribute('style', 'display:none');
-                svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-
-                // Add to the page
-                $('body').prepend(svgElement);
+        $.get({url: '/resources/icons.svg', dataType: 'text'})
+            .done((svgText) => {
+                $('#svgWrapper').html(svgText);
             });
     }
 };
